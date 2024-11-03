9efde44467e740e9f9bf7b0b2b48c713d5799f9d
ee2theipi <113263283+ee2theipi@users.noreply.github.com>
2024-11-03 23:14:44 +0530

    Update gold_prices.py

--- gold_prices.py
+++ gold_prices.py
@@ -1,1 +1,9 @@
+# https://www.goodreturns.in/gold-rates/
 
+import requests
+from bs4 import BeautifulSoup
+
+city = 'https://www.goodreturns.in/gold-rates/chandigarh.html'
+response = requests.get(gold_rate_city)
+
+#print(response)
